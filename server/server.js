7fea0ca4edd1d88b846c88c9a41bb249aa5552f1
Manuel Piatesi <mpiatesi@phase-eight.co.uk>
2018-08-20 17:25:10 +0100

    Add connect and disconnect events

--- server/server.js
+++ server/server.js
@@ -1,22 +1,32 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
-const app = express()
 const port = process.env.PORT || 3000
 
-//middleware
+const app = express()
+const server = http.createServer(app)
+//socket io config
+const io = socketIO(server)
 
 //*Serving static files in Express
 app.use(express.static(publicPath))
 
+//register an event listener
+io.on('connection', (socket) => {   //check server side if there is a connection
+    console.log('New user connected')
 
-// app.get('/', (req, res) => {
-//     res.send('HEllo world')
-// })
+    //check if a tab has been closed -- server side
+    socket.on('disconnect', () => {
+        console.log('User was disconnected from server')
+    })
+})
 
 
-app.listen(port, () => {
+//With http, si usa 'server' invece di 'app'
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
 
